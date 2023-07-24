53341fca7a23526f2e4380c1210842e29bda6a82
Nedal-haltam <133881380+Nedal-haltam@users.noreply.github.com>
2023-07-24 23:28:11 +0300

    Create source.cpp

---
+++ source.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<cstdlib>
+#include "database.h"
+
+using namespace std;
+
+//try it for fun
+
+
+void showlist()
+{
+	cout << "\t\t\thello , choose a choice :\n";
+	cout << "\t\t\t1-Login\n\t\t\t2-Register\n";
+	return;
+}
+
+
+int main()
+{
+	
+	base data;
+	while (1)
+	{
+		showlist();
+		int choice; cin >> choice; string username, password;
+		cout << "enter username: "; cin.ignore(); getline(cin, username);
+		cout << "enter password: "; cin.ignore(); getline(cin, password);
+
+		if (choice == 1)
+		{
+			data.login(username, password);
+		}
+		else if (choice == 2)
+		{
+			data.Register(username, password);
+		}
+		else cout << "wrong choice dont give up" << endl;
+
+	}
+}
